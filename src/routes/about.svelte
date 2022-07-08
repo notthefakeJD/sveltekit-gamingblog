2971440b0a8805e08d131baf486bf23f7e87a1b9
notthefakeJD <102332064@student.swin.edu.au>
2022-07-08 13:58:14 +1000

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<script>
+  
+</script>
+    
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil maiores, iste explicabo dolores dolorem atque, saepe sint doloremque praesentium eos id cum qui molestiae magni dolorum ratione distinctio illo quidem!</p>
+    <a href="/">Home</a>
+    <a href="/guides">Guides</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display:block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<div class="index">
+    <h2>Welcome</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, voluptas. Deleniti ad architecto dolor odit, sint totam at debitis obcaecati iste sapiente possimus, consectetur, quam quia. Vel error rem veniam.</p>
+    <a href="/about">About</a>
+    <a href="/guides">Guides</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display:block;
+        margin: 20px auto;
+    }
+</style>
